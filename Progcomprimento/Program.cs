4a272e3bfd6e026be0fe8adc0ac71c1e74143e47
Abner Casteliano <abnercasteliano@outlook.com>
2020-03-29 22:46:54 -0300

    inicio commit

---
+++ Progcomprimento/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Progcomprimento
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Comprimento comp = new Comprimento(200.0);
+
+            System.Console.WriteLine(comp.Metro + " metros");
+            System.Console.WriteLine(comp.Milha + " Milhas");
+
+            System.Console.ReadKey();
+        }
+    }
+}
